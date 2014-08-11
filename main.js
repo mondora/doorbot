3cb6e2fec86e082039f411ea0639dc7a2061a0f7
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-08-11 15:44:25 +0200

    Added console.log-s

--- main.js
+++ main.js
@@ -11,18 +11,22 @@ relay.on("ready", function relayReady () {
 	var srv = http.createServer();
 	srv.on("request", function (req, res) {
 		if (req.url !== "/open") {
+			console.log("UNAUTHORIZED");
 			res.writeHead(401, "UNAUTHORIZED");
 			res.end();
 			return;
 		}
 		if (open) {
+			console.log("RELAY ALREADY OPEN");
 			res.writeHead(200, "OK");
 			res.end();
 			return;
 		}
+		console.log("OPENING RELAY");
 		open = true;
 		relay.turnOn(1);
 		setTimeout(function () {
+			console.log("CLOSING RELAY");
 			open = false;
 			relay.turnOff(1);
 			res.writeHead(200, "OK");
